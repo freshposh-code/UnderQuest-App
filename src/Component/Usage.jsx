7d80c88451df9e53b418d20b86d0a95ce763f1df
freshposh-code <atomisefarouk919@gmail.com>
2023-06-30 02:16:31 +0100

    first commit

--- src/Component/Usage.jsx
+++ src/Component/Usage.jsx
@@ -10,7 +10,7 @@ const Usage = () => {
           <h1 className={`${styles.heading2} text-center`}>
             find everything used in{" "}
             <span className="text-red-500 sm:text-6xl text-4xl font-extrabold">
-              metaverse today
+              metaverse
           
             </span>
           </h1>
